05cb0595f11e88d06a15bd7302f1f2cce9d27c0d
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-08-08 14:47:46 +0300

    add comment index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const colors = [
 
 console.log(colors);
 
+let switchThemeTimeoutID = null;
+
 const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
   stopBtn: document.querySelector('[data-action="stop"]'),
@@ -23,7 +25,7 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-let switchTheme = null;
+// Меняем фон раз на рандомный цвет, используя фун-ию randomIntegerFromInterval
 
 function switchBgColor() {
   document.body.style.background =
@@ -31,14 +33,18 @@ function switchBgColor() {
   console.log('Меняю цвет фона на:', document.body.style.background);
 }
 
+// Запускаем изменение фона раз в 1 сек, блокируем кнопку Start, разблокируем Stop
+
 function startSwitchTheme() {
-  switchTheme = setInterval(switchBgColor, 1000);
+  switchThemeTimeoutID = setInterval(switchBgColor, 1000);
   refs.startBtn.setAttribute('disabled', true);
   refs.stopBtn.removeAttribute('disabled');
 }
 
+// Останавливаем изменение фона, разблокируем Start, блокируем Stop
+
 function stopSwitchTheme() {
-  clearInterval(switchTheme);
+  clearInterval(switchThemeTimeoutID);
   refs.startBtn.removeAttribute('disabled');
   refs.stopBtn.setAttribute('disabled', true);
   console.log('Останавливаю изменение фона');
