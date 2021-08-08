85fcec891fae28bce0a3597fe55e4600da609857
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-08-08 17:30:08 +0300

    add finally change block button

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ console.log(colors);
 
 const SWITCH_DELAY = 1000;
 let switchThemeTimeoutID = null;
+let switchIsActive = false;
 
 const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
@@ -28,25 +29,33 @@ const randomIntegerFromInterval = (min, max) => {
 // Меняем фон раз на рандомный цвет, используя фун-ию randomIntegerFromInterval
 
 function switchBgColor() {
-  document.body.style.background =
-    colors[randomIntegerFromInterval(0, colors.length - 1)];
-  console.log('Меняю цвет фона на:', document.body.style.background);
+  const randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+
+  document.body.style.background = colors[randomIndex];
+  console.log('Меняю цвет фона на:', colors[randomIndex]);
 }
 
 // Запускаем изменение фона раз в 1 сек, блокируем кнопку Start, разблокируем Stop
 
 function startSwitchTheme() {
+  if (switchIsActive) {
+    return;
+  }
+
   switchThemeTimeoutID = setInterval(switchBgColor, SWITCH_DELAY);
-  refs.startBtn.setAttribute('disabled', true);
-  refs.stopBtn.removeAttribute('disabled');
+  switchIsActive = true;
 }
 
 // Останавливаем изменение фона, разблокируем Start, блокируем Stop
 
 function stopSwitchTheme() {
+  if (!switchIsActive) {
+    return;
+  }
+
   clearInterval(switchThemeTimeoutID);
-  refs.startBtn.removeAttribute('disabled');
-  refs.stopBtn.setAttribute('disabled', true);
+  switchIsActive = false;
+
   console.log('Останавливаю изменение фона');
 }
 
